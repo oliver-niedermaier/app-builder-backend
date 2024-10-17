5dc355b9e4c83d0f5c71e234b09e0ff42a5d90f0
Oliver Niedermaier <oliver.niedermaier@unitedplanet.com>
2024-10-17 13:51:20 +0200

    ✨ Setup nuxt-content and create simple component that reads our `app.data.yml`

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+        <FetchData />
+
         <NuxtRouteAnnouncer />
         <NuxtWelcome />
     </div>
---
+++ components/FetchData.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>{{ app.title }}</h1>
+        <strong>Pages:</strong>
+        <ul>
+            <li v-for="page in app.structure.items" :key="page.id">
+                <code> 🧬 {{ Object.keys(page)[0] }} </code>
+                <code> 🆔 {{ page.id }} </code>
+                <code> 🏷️ {{ page.title }} </code>
+                <code> 🔗 {{ page.route }} </code>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+const { app } = await queryContent("data/app.data").findOne();
+</script>
