0641c7ff05f824b53581539650355033056a1c07
Oliver Niedermaier <oliver.niedermaier@unitedplanet.com>
2024-10-17 16:42:09 +0200

    ✨ Implement PrimeVue and render the whole app-structure within a `TreeTable` component

--- components/FetchData.vue
+++ components/FetchData.vue
@@ -1,18 +1,56 @@
 <template>
-    <div>
-        <h1>{{ app.title }}</h1>
-        <strong>Pages:</strong>
-        <ul>
-            <li v-for="page in app.structure.items" :key="page.id">
-                <code> 🧬 {{ Object.keys(page)[0] }} </code>
-                <code> 🆔 {{ page.id }} </code>
-                <code> 🏷️ {{ page.title }} </code>
-                <code> 🔗 {{ page.route }} </code>
-            </li>
-        </ul>
+    <div style="padding: 1em">
+        <h1>
+            {{ app.title }} <code>{{ app.id }}</code>
+        </h1>
+
+        <!-- plain html -->
+        <div style="display: none">
+            <ul>
+                <li v-for="page in app.structure.items" :key="page.id">
+                    <code> 🧬 {{ Object.keys(page)[0] }} </code>
+                    <code> 🆔 {{ page.id }} </code>
+                    <code> 🏷️ {{ page.title }} </code>
+                    <code> 🔗 {{ page.route }} </code>
+                </li>
+            </ul>
+        </div>
+
+        <!-- prime vue component -->
+        <TreeTable
+            :value="treeNodes"
+            size="small"
+            resizable-columns="true"
+            data-key="id"
+            row-hover="true"
+            show-gridlines
+        >
+            <Column field="title" header="🏷️ Title" expander />
+            <Column field="type" header="🧬 Type" />
+            <Column field="id" header="🆔 ID" />
+            <Column field="route" header="🔗 Route" />
+        </TreeTable>
     </div>
 </template>
 
 <script setup>
+// import app data yaml file
 const { app } = await queryContent("data/app.data").findOne();
+
+// function to transform items into TreeNode format
+// only needed to fit to primevue TreeTable component
+function generateTreeNodes(items) {
+    return items.map((item) => ({
+        data: {
+            id: item.id,
+            title: item.title,
+            type: Object.keys(item)[0],
+            route: item.route,
+            expander: item.items ? true : false,
+        },
+        children: item.items ? generateTreeNodes(item.items) : null,
+    }));
+}
+
+const treeNodes = ref(generateTreeNodes(app.structure.items));
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from "@primevue/themes/aura";
+
 export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
     devtools: { enabled: true },
-    modules: ["@nuxt/eslint", "@nuxt/content"],
+    modules: ["@nuxt/eslint", "@nuxt/content", "@primevue/nuxt-module"],
+    primevue: {
+        options: {
+            theme: {
+                preset: Aura,
+            },
+        },
+    },
 });
