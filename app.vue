93230bfd2b9118aaeb095a0a10a122ef4f075908
Oliver Niedermaier <oliver.niedermaier@unitedplanet.com>
2024-10-17 17:51:17 +0200

    🔧 Apply some meta data (app title + favicon)

--- app.vue
+++ app.vue
@@ -3,6 +3,22 @@
         <FetchData />
 
         <NuxtRouteAnnouncer />
-        <NuxtWelcome />
+        <NuxtWelcome :title="appTitle" :app-name="appName" />
     </div>
 </template>
+
+<script setup lang="ts">
+const appName = "app-builder-nuxt";
+const appTitle = "App Builder · Nuxt";
+
+useHead({
+    title: appTitle,
+    link: [
+        {
+            rel: "icon",
+            type: "image/svg+xml",
+            href: "/mage.svg",
+        },
+    ],
+});
+</script>
